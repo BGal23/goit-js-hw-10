9c98692c8f9d9269fefdd0deeb0a6387c37f9b60
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-09-28 23:50:29 +0200

    need corect

---
+++ src/cat-api.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+axios.defaults.headers.common["live_oiJWdZviVlbnAIfHszSzp2umobU27xezVTg2ey2mGvs5e8u3PEIDjOEU4QlU78Wp"] = "key"
+
+const basic = "https://api.thecatapi.com/v1/"
+const selectCat = document.querySelector(".breed-select");
+const catBox = document.querySelector(".cat-info")
+let catInfo;
+
+fetchBreeds = () => {
+    fetch(`${basic}breeds`)
+        .then(response => {
+            if(!response.ok){
+                throw new Error(response.status)
+            }
+            return response.json()
+        })
+        .then(data => {
+            data.forEach(element => {
+                const newCat = document.createElement("option");
+                selectCat.append(newCat)
+                newCat.value = element.id
+                newCat.textContent = element.name
+            });
+        })
+        .catch(error => {
+            console.log(error)
+        })
+        return
+}
+
+const fetchCatByBreed = (breedId) => {
+    fetch(`${basic}images/search?breed_ids=${breedId}`)
+        .then(response => {
+            if(!response.ok){
+                throw new Error(response.status)
+            }
+            return response.json()
+        })
+        .then(data => {
+            [catObj] = data
+            catBox.innerHTML = `
+            <img src="${catObj.url}"height="400"></img>`
+        })
+        .catch(error => {
+            console.log(error)
+        })
+        return
+}
+
+selectCat.addEventListener("input", () => fetchCatByBreed(selectCat.value))
--- src/index.js
+++ src/index.js
@@ -1,34 +1,3 @@
-import axios from "axios";
-axios.defaults.headers.common["live_oiJWdZviVlbnAIfHszSzp2umobU27xezVTg2ey2mGvs5e8u3PEIDjOEU4QlU78Wp"] = "key"
+import * as catApi from "./cat-api"
 
-const selectCat = document.querySelector(".breed-select");
-
-const options = {
-    //"x-api-key": "key"
-}
-const fetchBreeds = () => {
-    fetch("https://api.thecatapi.com/v1/breeds", options)
-        .then(response => {
-            if(!response.ok){
-                throw new Error(response.status)
-            }
-            return response.json()
-        })
-        .then(data => {
-            data.forEach(element => {
-                const newCat = document.createElement("option");
-                selectCat.append(newCat)
-                newCat.value = element.id
-                newCat.textContent = element.name
-            });
-        })
-        .catch(error => {
-            console.log(error)
-        })
-}
-
-const showCat = () => {
-    console.log(selectCat.value)
-}
 fetchBreeds()
-selectCat.addEventListener("input", showCat)
