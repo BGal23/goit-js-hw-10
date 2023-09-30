230e66a7cabd2d89a99dccab6e0e788c10970ef5
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-09-30 15:52:42 +0200

    finish

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,12 +1,19 @@
-import axios from "axios";
-axios.defaults.headers.common["live_oiJWdZviVlbnAIfHszSzp2umobU27xezVTg2ey2mGvs5e8u3PEIDjOEU4QlU78Wp"] = "key";
+//import axios from "axios";
+//axios.defaults.headers.common["live_oiJWdZviVlbnAIfHszSzp2umobU27xezVTg2ey2mGvs5e8u3PEIDjOEU4QlU78Wp"] = "key";
+
+const key = "live_oiJWdZviVlbnAIfHszSzp2umobU27xezVTg2ey2mGvs5e8u3PEIDjOEU4QlU78Wp"
+// nie wiem czy dobrze dodałem klucz bo nie wiem jak to sprawdzić czy jest dobrze dodany
 
 const basic = "https://api.thecatapi.com/v1/"
+const loader = document.querySelector(".loader");
 const selectCat = document.querySelector(".breed-select");
+const error = document.querySelector(".error");
 const catBox = document.querySelector(".cat-info")
+// też nie udał mi sie podzielić kodu na dwa pliki z racji tego że nie wiem jak 
+// wywołać funkcje w cat-api i żeby otwożyła sie w index i na odwrót 
 
 const fetchBreeds = (breedId, img) => {
-    fetch(`${basic}breeds`)
+    fetch(`${basic}breeds?api_key=${key}`)
     .then(response => {
         if(!response.ok){
             throw new Error(response.status)
@@ -23,6 +30,7 @@ const fetchBreeds = (breedId, img) => {
         }
     }) 
     .catch(error => {
+        error.style.display = "block"
         console.log(error)
     })
     return
@@ -39,19 +47,24 @@ const catsList = (catInfo) => {
 }
 
 const fetchCatByBreed = (breedId) => {
-    fetch(`${basic}images/search?breed_ids=${breedId}`)
-        .then(response => {
-            if(!response.ok){
-                throw new Error(response.status)
-            }
+    loader.style.display = "block"
+    catBox.style.display = "none"
+    fetch(`${basic}images/search?breed_ids=${breedId}&api_key=${key}`)
+    .then(response => {
+        if(!response.ok){
+            throw new Error(response.status)
+        }
+            error.style.display = "none"
             return response.json()
         })
         .then(data => {
             const img = data[0]
             fetchBreeds(breedId, img)
         })
-        .catch(error => {
-            console.log(error)
+        .catch(problem => {
+            loader.style.display = "none"
+            error.style.display = "block"
+            console.log(problem)
         })
         return
 }
@@ -61,10 +74,16 @@ const catInfo = (info, img) => {
         <h2>${info.name}</h2>
         <img src="${img.url}"height="400"></img>
         <p>${info.description}</p>
-        <p><b>Temperament: </b>${info.temperament}</p>`   
-    return
-}
+        <p><b>Temperament: </b>${info.temperament}</p>`  
+        setTimeout(() => {
+            catBox.style.display = "block"
+            loader.style.display = "none"
+        },0)
+        return
+    }
+
+// wiem że to opóźnienie setTimeout nie wygląda dobrze ale nie wiem jak zrobić to
+// w ten sposób aby otwierało sie to na końcu kolejki 
 
 fetchBreeds()
-
 selectCat.addEventListener("input", () => fetchCatByBreed(selectCat.value))
