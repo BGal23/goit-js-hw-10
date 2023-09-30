470e764e4f1e2c8e2bc6712820bdc9858ccfc78f
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-09-30 13:45:58 +0200

    almost done

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,31 +1,40 @@
 import axios from "axios";
-axios.defaults.headers.common["live_oiJWdZviVlbnAIfHszSzp2umobU27xezVTg2ey2mGvs5e8u3PEIDjOEU4QlU78Wp"] = "key"
+axios.defaults.headers.common["live_oiJWdZviVlbnAIfHszSzp2umobU27xezVTg2ey2mGvs5e8u3PEIDjOEU4QlU78Wp"] = "key";
 
 const basic = "https://api.thecatapi.com/v1/"
 const selectCat = document.querySelector(".breed-select");
 const catBox = document.querySelector(".cat-info")
-let catObj = {};
 
-const fetchBreeds = () => {
+const fetchBreeds = (breedId, img) => {
     fetch(`${basic}breeds`)
-        .then(response => {
-            if(!response.ok){
-                throw new Error(response.status)
-            }
-            return response.json()
-        })
-        .then(data => {
-            data.forEach(element => {
-                const newCat = document.createElement("option");
-                selectCat.append(newCat)
-                newCat.value = element.id
-                newCat.textContent = element.name
-            });
-        })
-        .catch(error => {
-            console.log(error)
-        })
-        return
+    .then(response => {
+        if(!response.ok){
+            throw new Error(response.status)
+        }
+        return response.json()
+    })
+    .then(data => {
+        if (!breedId) {
+            catsList(data)
+        }
+        else {
+            const info = data.find(option => option.id === breedId)
+            catInfo(info, img)
+        }
+    }) 
+    .catch(error => {
+        console.log(error)
+    })
+    return
+}
+
+const catsList = (catInfo) => {
+    catInfo.forEach(element => {
+        const newCat = document.createElement("option");
+        selectCat.append(newCat)
+        newCat.value = element.id
+        newCat.textContent = element.name
+    });
 }
 
 const fetchCatByBreed = (breedId) => {
@@ -37,14 +46,21 @@ const fetchCatByBreed = (breedId) => {
             return response.json()
         })
         .then(data => {
-            [catObj] = data
-            catBox.innerHTML = `
-            <img src="${catObj.url}"height="400"></img>`
+            const img = data[0]
+            fetchBreeds(breedId, img)
         })
         .catch(error => {
             console.log(error)
         })
         return
+}
+
+const catInfo = (info, img) => {
+    catBox.innerHTML = `
+        <h2>${info.name}</h2>
+        <img src="${img.url}"height="400"></img>
+        <p>${info.description}</p>
+        <p>${info.temperament}</p>`   
 }
 
 fetchBreeds()
--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
 import * as catApi from "./cat-api"
-
